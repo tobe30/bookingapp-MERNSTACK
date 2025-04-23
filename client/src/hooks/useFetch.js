587b79d83185b4874c879bcea4713f216c19c092
tobe <tobemarizu@gmail.com>
2025-04-23 19:49:18 +0100

    Fix: Added admin and client as normal folders

---
+++ client/src/hooks/useFetch.js
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const useFetch = (url) => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const res = await axios.get(url);
+        setData(res.data);
+      } catch (err) {
+        setError(err);
+      }
+      setLoading(false);
+    };
+    fetchData();
+  }, [url]);// whenever we change the url is going to refetch again//remove url to change effect
+
+  const reFetch = async () => {
+    setLoading(true);
+    try {
+      const res = await axios.get(url);
+      setData(res.data);
+    } catch (err) {
+      setError(err);
+    }
+    setLoading(false);
+  };
+
+  return { data, loading, error, reFetch };
+};
+
+export default useFetch;
